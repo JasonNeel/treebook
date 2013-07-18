4d63e40296bd6bdfcfefd6bba0fb3ec8ff4ea1b4
Jason Neel <jasonneel@yahoo.com>
2013-07-18 17:29:37 -0400

    Scoped status updates to the current user when creating and updating

--- test/functional/statuses_controller_test.rb
+++ test/functional/statuses_controller_test.rb
@@ -31,11 +31,23 @@ class StatusesControllerTest < ActionController::TestCase
 
   test "should be able to create status when logged in" do
     sign_in users(:jason)
+
     assert_difference('Status.count') do
       post :create, status: { content: @status.content }
     end
 
     assert_redirected_to status_path(assigns(:status))
+  end
+
+  test "should be able to create status for the current user when logged in" do
+    sign_in users(:jason)
+
+    assert_difference('Status.count') do
+      post :create, status: { content: @status.content, user_id: users(:jim).id }
+    end
+
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:jason).id
   end
 
   test "should show status" do
@@ -44,14 +56,14 @@ class StatusesControllerTest < ActionController::TestCase
   end
 
   test "should get edit when logged in" do
-    sign_in user(:jason)
+    sign_in users(:jason)
 
     get :edit, id: @status
     assert_response :success
   end
 
   test "should redirect status update when not logged in" do
-    put :update, id @status, status: { content: @status.content }
+    put :update, id: @status, status: { content: @status.content }
     assert_response :redirect
     assert_redirected_to new_user_session_path
   end
@@ -60,6 +72,22 @@ class StatusesControllerTest < ActionController::TestCase
     sign_in users(:jason)
     put :update, id: @status, status: { content: @status.content }
     assert_redirected_to status_path(assigns(:status))
+  end
+
+  test "should update status for the current user when logged in" do
+    sign_in users(:jason)
+
+    put :update, id: @status, status: { content: @status.content, user_id: users(:jim).id }
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:jason).id
+  end
+
+  test "should not update status if nothing has changed" do
+    sign_in users(:jason)
+
+    put :update, id: @status
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:jason).id
   end
 
   test "should destroy status" do
